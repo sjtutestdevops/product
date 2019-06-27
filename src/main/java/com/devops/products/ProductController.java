b85e38f3e47db150a9555a5c53587759a176b854
supperai <jhb19940603@163.com>
2019-06-27 16:46:35 +0800

    test@deploy

--- src/main/java/com/devops/products/ProductController.java
+++ src/main/java/com/devops/products/ProductController.java
@@ -1,5 +1,11 @@
 package com.devops.products;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,6 +24,32 @@ public class ProductController {
     @RequestMapping(value = "/test", method = RequestMethod.GET)
     public String test() {
         return userService.test();
+    }
+
+    @RequestMapping(value = "/test1", method = RequestMethod.GET)
+    public String test1() throws Exception{
+        CloseableHttpClient client = null;
+        CloseableHttpResponse response = null;
+        String url = "http://30472.grff4a78.9upmwa3t.590bc8.grapps.cn/test1";
+        try {
+            HttpGet httpGet = new HttpGet(url);
+
+            client = HttpClients.createDefault();
+            response = client.execute(httpGet);
+            HttpEntity entity = response.getEntity();
+            String result = EntityUtils.toString(entity);
+            return result;
+        } catch (Exception e) {
+            return e.getMessage();
+        } finally {
+            if (response != null) {
+                response.close();
+            }
+            if (client != null) {
+                client.close();
+            }
+        }
+
     }
 
 //    查看主要成分
